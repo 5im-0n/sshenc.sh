eed415d7b28c92657f1cfe0877c39122b984d17f
s2 <s2@31337.it>
2019-11-26 16:53:44 +0100

    add more tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,11 @@
 cleanup() {
-    rm -rf "$tempfile"
+    rm -rf "$temp_dir"
 }
 
 trap cleanup EXIT
 
-
-tempfile=$(tempfile)
+temp_dir="$(mktemp -d -t "sshenc.sh.XXXXXX")"
+tempfile="$(mktemp "$temp_dir/sshenc.sh.XXXXXX.cypher")"
 plaintext=$(cat sometext)
 
 echo -n 'testing multiple pubkeys: '
@@ -19,6 +19,29 @@ for i in {1..3}; do
         echo -n "key$i: ⛝ "
     fi
 done
+echo
+
+echo -n 'testing encryption with a single key: '
+../sshenc.sh -p id_rsa-1.pub < sometext > $tempfile
+cyph=$(../sshenc.sh -s id_rsa-1 < $tempfile)
+if [ "$cyph" == "$plaintext" ]; then
+    echo -n "✓"
+else
+    echo -n "⛝"
+fi
+echo
+
+echo -n 'testing encryption of a binary file: '
+../sshenc.sh -p id_rsa-1.pub < ../logo.png > $tempfile
+../sshenc.sh -s id_rsa-1 < $tempfile > $temp_dir/binary
+diff ../logo.png $temp_dir/binary
+retval=$?
+if [ $retval -eq 0 ]; then
+    echo -n "✓"
+else
+    echo -n "⛝"
+fi
+echo
 
 echo
 echo done.
